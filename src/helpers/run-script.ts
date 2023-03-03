5bd5f38135ac147177eb33cd8b9011353c77ec24
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2023-03-03 02:21:24 +0100

    Server: Use spawn instead of exec for scripts

--- src/helpers/run-script.ts
+++ src/helpers/run-script.ts
@@ -1,10 +1,25 @@
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 import path from 'path';
-import { promisify } from 'util';
-import { getLogger } from './logger';
 import { getScriptRoot } from './util';
 
-export const runSudoScript = async (script: string, ...args: string[]) => {
+export const runSudoScript = (script: string, ...args: string[]): Promise<{ stderr: string; stdout: string }> => {
 	const scriptRoot = getScriptRoot();
-	return await promisify(exec)(`sudo ${path.join(scriptRoot, script)} ${args.join(' ')}`, { env: process.env });
+	return new Promise((resolve, reject) => {
+		const child = spawn(`sudo ${path.join(scriptRoot, script)}`, args);
+		let stdout = '';
+		let stderr = '';
+		child.stdout.on('data', (data) => {
+			stdout += data;
+		});
+		child.stderr.on('data', (data) => {
+			stderr += data;
+		});
+		child.on('close', (code) => {
+			if (code === 0) {
+				resolve({ stderr, stdout });
+			} else {
+				reject(stderr);
+			}
+		});
+	});
 };
